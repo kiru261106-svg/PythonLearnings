b509f02f1bcd3448fcd5863a8b1f3c217cc24c42
kiru261106-svg <kiru261106@gmail.com>
2025-09-10 16:38:03 +0530

    Python Files
    Uploading Python files

---
+++ calculation.py
@@ -0,0 +1,9 @@
+tamil=int(input("enter the tamil mark"))
+english=int(input("enter the english mark"))
+maths=int(input("enter the maths mark"))
+computerscience=int(input("enter the computerscinece mark"))
+total=tamil+english+maths+computerscience
+print(f"The total mark is :::::: {tamil+english+maths+computerscience}")
+print(f"The total mark total variable  :::::: {total}")
+percentage=total/400
+print(f"the percentage is::: {percentage}")
